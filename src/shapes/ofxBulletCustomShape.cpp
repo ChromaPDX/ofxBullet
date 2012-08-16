49327fdee193182cb4cd1e25ce343405ddb8334d
Matas Ubarevicius <m.ubarevicius@gmail.com>
2012-08-17 01:28:15 +0200

    If Convex Hull false, then use Custom Mesh

--- src/shapes/ofxBulletCustomShape.cpp
+++ src/shapes/ofxBulletCustomShape.cpp
@@ -16,7 +16,7 @@ ofxBulletCustomShape::ofxBulletCustomShape() {
 
 //--------------------------------------------------------------
 ofxBulletCustomShape::~ofxBulletCustomShape() {
-	
+
 }
 
 // pass in an already created compound shape //
@@ -73,13 +73,13 @@ bool ofxBulletCustomShape::addMesh( ofMesh a_mesh, ofVec3f a_localScaling, bool 
 		ofLog( OF_LOG_ERROR, "ofxBulletCustomShape :: addMesh : can not call after calling add()" );
 		return false;
 	}
-	
+
 	btVector3 localScaling( a_localScaling.x, a_localScaling.y, a_localScaling.z );
 	vector <ofIndexType>	indicies = a_mesh.getIndices();
 	vector <ofVec3f>		verticies = a_mesh.getVertices();
-	
+
 	btVector3 centroid = btVector3(0, 0, 0);
-	
+
 	if(!a_bUseConvexHull) {
 		for(int i = 0; i < verticies.size(); i++) {
 			btVector3 tempVec = btVector3(verticies[i].x, verticies[i].y, verticies[i].z);
@@ -87,69 +87,80 @@ bool ofxBulletCustomShape::addMesh( ofMesh a_mesh, ofVec3f a_localScaling, bool 
 			centroid += tempVec;
 		}
 		centroid /= (float)verticies.size();
-		
-		vector<btVector3> newVerts;
-		for ( int i = 0; i < indicies.size(); i++) {
-			btVector3 vertex( verticies[indicies[i]].x, verticies[indicies[i]].y, verticies[indicies[i]].z);
-			vertex *= localScaling;
-			vertex -= centroid;
-			newVerts.push_back(vertex);
-		}
-		
-		btConvexHullShape* convexShape = new btConvexHullShape(&(newVerts[0].getX()), newVerts.size());
-		convexShape->setMargin( 0.01f );
-		shapes.push_back( convexShape );
-		centroids.push_back( ofVec3f(centroid.getX(), centroid.getY(), centroid.getZ()) );
-	} else {
-		// HULL Building code from example ConvexDecompositionDemo.cpp //
-		btTriangleMesh* trimesh = new btTriangleMesh();
-		
+
+
+        btTriangleMesh* trimesh = new btTriangleMesh();
+
 		for ( int i = 0; i < indicies.size()/3; i++) {
 			int index0 = indicies[i*3];
 			int index1 = indicies[i*3+1];
 			int index2 = indicies[i*3+2];
-			
+
 			btVector3 vertex0( verticies[index0].x, verticies[index0].y, verticies[index0].z );
 			btVector3 vertex1( verticies[index1].x, verticies[index1].y, verticies[index1].z );
 			btVector3 vertex2( verticies[index2].x, verticies[index2].y, verticies[index2].z );
-			
+
 			vertex0 *= localScaling;
 			vertex1 *= localScaling;
 			vertex2 *= localScaling;
-			
+
 			trimesh->addTriangle(vertex0, vertex1, vertex2);
 		}
-		
+
+        btBvhTriangleMeshShape *shapetr = new btBvhTriangleMeshShape(trimesh, false);
+
+		shapes.push_back( shapetr );
+		centroids.push_back( ofVec3f(centroid.getX(), centroid.getY(), centroid.getZ()) );
+	} else {
+		// HULL Building code from example ConvexDecompositionDemo.cpp //
+		btTriangleMesh* trimesh = new btTriangleMesh();
+
+		for ( int i = 0; i < indicies.size()/3; i++) {
+			int index0 = indicies[i*3];
+			int index1 = indicies[i*3+1];
+			int index2 = indicies[i*3+2];
+
+			btVector3 vertex0( verticies[index0].x, verticies[index0].y, verticies[index0].z );
+			btVector3 vertex1( verticies[index1].x, verticies[index1].y, verticies[index1].z );
+			btVector3 vertex2( verticies[index2].x, verticies[index2].y, verticies[index2].z );
+
+			vertex0 *= localScaling;
+			vertex1 *= localScaling;
+			vertex2 *= localScaling;
+
+			trimesh->addTriangle(vertex0, vertex1, vertex2);
+		}
+
 		//cout << "ofxBulletCustomShape :: addMesh : input triangles = " << trimesh->getNumTriangles() << endl;
 		//cout << "ofxBulletCustomShape :: addMesh : input indicies = " << indicies.size() << endl;
 		//cout << "ofxBulletCustomShape :: addMesh : input verticies = " << verticies.size() << endl;
-		
+
 		btConvexShape* tmpConvexShape = new btConvexTriangleMeshShape(trimesh);
-		
+
 		//create a hull approximation
 		btShapeHull* hull = new btShapeHull(tmpConvexShape);
 		btScalar margin = tmpConvexShape->getMargin();
 		hull->buildHull(margin);
 		tmpConvexShape->setUserPointer(hull);
-		
+
 		centroid = btVector3(0., 0., 0.);
 		for (int i = 0; i < hull->numVertices(); i++) {
 			centroid += hull->getVertexPointer()[i];
 		}
 		centroid /= (float)hull->numVertices();
-		
+
 		//printf("ofxBulletCustomShape :: addMesh : new hull numTriangles = %d\n", hull->numTriangles());
 		//printf("ofxBulletCustomShape :: addMesh : new hull numIndices = %d\n", hull->numIndices());
 		//printf("ofxBulletCustomShape :: addMesh : new hull numVertices = %d\n", hull->numVertices());
-		
+
 		btConvexHullShape* convexShape = new btConvexHullShape();
 		for (int i=0;i<hull->numVertices();i++) {
 			convexShape->addPoint(hull->getVertexPointer()[i] - centroid);
 		}
-		
+
 		delete tmpConvexShape;
 		delete hull;
-		
+
 		shapes.push_back( convexShape );
 		centroids.push_back( ofVec3f(centroid.getX(), centroid.getY(), centroid.getZ()) );
 	}
@@ -161,7 +172,7 @@ void ofxBulletCustomShape::add() {
 	_bAdded = true;
 	btTransform trans;
 	trans.setIdentity();
-	
+
 	for(int i = 0; i < centroids.size(); i++) {
 		_centroid += centroids[i];
 	}
@@ -196,7 +207,7 @@ int ofxBulletCustomShape::getNumChildShapes() {
 
 //--------------------------------------------------------------
 void ofxBulletCustomShape::draw() {
-	
+
 }
 
 
